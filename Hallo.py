e0f3766886b9d4c211f38a38a7ac9c25cb62f9a6
Jens Hahn <jens.hahn@hu-berlin.de>
2018-04-23 15:14:28 +0200

    Changed lecture slides for git intro

---
+++ Hallo.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 23 18:13:18 2018
+
+@author: gschreiber
+"""
+x='Gabi'
+print ('Hallo '  + x + '! Schön dich kennenzulernen!')
+
+print ('Hallo Gabi')
+
